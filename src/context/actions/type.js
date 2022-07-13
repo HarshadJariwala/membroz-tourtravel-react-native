24b068faa1b4fdf0f9306f8d59ed02a37ca2c643
HarshadJariwala <harshadjariwala1998@gmail.com>
2022-07-13 12:31:30 +0530

    uodate

--- src/context/actions/type.js
+++ src/context/actions/type.js
@@ -10,7 +10,7 @@ export const CLOUD_URL = 'https://api.cloudinary.com/v1_1/membroz/upload';
 export const UPLOAD_PRESET = 'egi2wepr';
 export const MESSAGEKEY = "key=AAAAEnSG7us:APA91bF_fsNvJ-RoDW56GfT8wyg4nYt78wBlcsyVb4Sa5kzqVWHLU-kWsueU9HcOoOX6qF8Esu9BoCHPNvTT6zntXmOd6UQ-ygrPxP42ldjwqDH0DzW5U2bf4UlPXL1NswPsLFaRNT3x"
 export const DEFAULTPROFILE = 'https://res.cloudinary.com/dnogrvbs2/image/upload/v1613538969/profile1_xspwoy.png';
-export const REMOTECONFIGKEY = '_restaurant_membroz_detail';
+export const REMOTECONFIGKEY = '_tour_membroz_detail';
 export const MESSAGINGSENDERID = "24932558592";
 export const ADDTOCARDLISTINFO = '@addtocardlist';
 export const AUTHLANGUAGE = '@authlanguage';
